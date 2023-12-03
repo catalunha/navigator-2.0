258d9de49ac521fd9e40d8fbfbbc646d36813ac4
catalunha <catalunha.mj@gmail.com>
2023-12-03 10:07:15 -0300

    com transitionDuration

--- lib/app/routes.dart
+++ lib/app/routes.dart
@@ -60,30 +60,11 @@ Route? homeOnGenerateRoute(RouteSettings settings) {
             arg: argu,
           ),
           direction: AxisDirection.up);
-
-    // return PageRouteBuilder(
-    //     pageBuilder: (context, animation, secondaryAnimation) => ThirdPage(
-    //           arg: argu,
-    //         ),
-    //     transitionsBuilder: (context, animation, secondaryAnimation, child) {
-    //       const begin = Offset(0.0, 1.0);
-    //       const end = Offset.zero;
-    //       const curve = Curves.ease;
-
-    //       var tween =
-    //           Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
-
-    //       return SlideTransition(
-    //         position: animation.drive(tween),
-    //         child: child,
-    //       );
-    //     });
     default:
       return MaterialPageRoute(
         builder: (_) => const SizedBox.shrink(),
       );
   }
-  // return null;
 }
 
 class CustomPageRoute extends PageRouteBuilder {
